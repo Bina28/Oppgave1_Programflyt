3c3089588bcf1545644b30500a79a6fbea1ccfc8
Bina <dyakovabina@gmail.com>
2025-12-01 14:18:50 +0100

    Init commit

---
+++ Oppgave1_Programflyt/UserInteraction.cs
@@ -0,0 +1,21 @@
+﻿namespace Oppgave1_Programflyt;
+
+public class UserInteraction
+{
+
+    public static string UserNameInput()
+    {
+        string? input;
+        do
+        {
+            Console.WriteLine("Please enter your name bellow \n");
+            input = Console.ReadLine();
+        } while (string.IsNullOrEmpty(input));
+        return input;
+    }
+
+    public static void WelcomeMessage(string name)
+    {
+        Console.WriteLine($"Hello {name}. Welcome to the MOOD APP!");
+    }
+}
